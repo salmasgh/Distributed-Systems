fbab404f8b82f13b4a0dc3db4d7b3fb935c2ebac
salmasgh <selma.sghaier@insat.ucar.tn>
2025-03-10 12:59:35 +0100

    first commit

---
+++ TP1/DeliverCallback.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import com.rabbitmq.client.Delivery;
+
+@FunctionalInterface
+public interface DeliverCallback {
+
+    /**
+     * Called when a <code><b>basic.deliver</b></code> is received for this consumer.
+     *
+     * @param consumerTag the <i>consumer tag</i> associated with the consumer
+     * @param message the delivered message
+     * @throws IOException if the consumer encounters an I/O error while processing the message
+     */
+    void handle(String consumerTag, Delivery message) throws IOException;
+}
